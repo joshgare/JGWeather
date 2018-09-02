0f96b7f9cde5ead5b83910144d53c2be773ffb49
joshgare <josh@jgapplications.com>
2018-09-02 22:11:24 +0100

    add: documentation for APIClient

--- JGWeather/APIClient/APIClient.swift
+++ JGWeather/APIClient/APIClient.swift
@@ -19,6 +19,12 @@ class APIClient {
     private var serverUrl: String = "https://api.darksky.net/forecast"
     private var apiKey: String = "bdedc120084b8862558aeb732a44510c" // Please note this key may expire / be suspened and you can generate a new key at https://darksky.net/dev
     
+    
+    /**
+     Retrieves the current forecast data for the specified location.
+     
+     - Returns: A new Forecast of the forecast data, a success bool and an error object.
+     */
     func retrieveForecast(with location: CLLocation, completionHandler: @escaping (_ success: Bool, _ forecast: JGForecast?, _ error: Error?) -> Void) {
         Alamofire.request("\(serverUrl)/\(apiKey)/\(location.coordinate.latitude),\(location.coordinate.longitude)").responseJGForecast { response in
             if let error = response.error {
@@ -31,6 +37,11 @@ class APIClient {
         }
     }
     
+    /**
+     Retrieves the current forecast data for the current location.
+     
+     - Returns: A new Forecast of the forecast data, a success bool and an error object.
+     */
     func retrieveForecastForCurrentLocation(completionHandler: @escaping (_ success: Bool, _ forecast: JGForecast?, _ error: Error?) -> Void) {
         let locationManager: INTULocationManager = INTULocationManager.sharedInstance()
         locationManager.requestLocation(withDesiredAccuracy: .city, timeout: 10.0, delayUntilAuthorized: true) { (currentLocation, achievedAccuracy, status) in
