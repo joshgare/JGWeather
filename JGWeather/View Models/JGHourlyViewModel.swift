1f48a4b9c7c94fb243eab44ddd0cc247fa680f64
joshgare <josh@jgapplications.com>
2018-09-02 17:31:52 +0100

    add: definitions for our view models

---
+++ JGWeather/View Models/JGHourlyViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  JGHourlyViewModel.swift
+//  JGWeather
+//
+//  Created by Joshua Gare on 02/09/2018.
+//  Copyright © 2018 Josh Gare. All rights reserved.
+//
+
+import Foundation
+
+class JGHourlyViewModel {
+    
+}
