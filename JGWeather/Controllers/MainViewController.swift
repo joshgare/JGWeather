e51361bb3286976a6458fa5e53483d6167133fc7
joshgare <josh@jgapplications.com>
2018-09-02 17:26:00 +0100

    add: new UITableView and appropriate delegate methods ready for cells

--- JGWeather/Controllers/MainViewController.swift
+++ JGWeather/Controllers/MainViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var layoutTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +21,14 @@ class MainViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
 
